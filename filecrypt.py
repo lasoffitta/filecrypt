6a9d1f31e005b9ef5e35045e5fa503f57aa24da0
lasoffitta <165275950+lasoffitta@users.noreply.github.com>
2024-04-01 00:44:02 +0200

    Update filecrypt.py

--- filecrypt.py
+++ filecrypt.py
@@ -2,6 +2,9 @@ from flask import Flask, request
 from telegram import Update, Bot
 from telegram.ext import Updater, CommandHandler, CallbackContext, Dispatcher
 import os
+import requests
+from bs4 import BeautifulSoup
+import json
 
 TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 bot = Bot(TOKEN)
@@ -30,6 +33,30 @@ def link(update: Update, context: CallbackContext) -> None:
             update.message.reply_text(link)
     else:
         update.message.reply_text('Per favore, fornisce un URL valido.')
+
+def get_links(filecrypt_url):
+    response = requests.get(filecrypt_url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    if "/Link/" in filecrypt_url:
+        script = soup.find_all('script')[-1].string
+        start = script.rfind('http')
+        end = script.find('id=', start) + 43
+        link = script[start:end].replace('&amp;', '&')
+        return [link]
+    elif "/Container/" in filecrypt_url:
+        dlcdownload_element = soup.find('button', class_='dlcdownload')
+        if dlcdownload_element is None:
+            print("Could not find the dlcdownload element.")
+            return []
+        dlc_id = dlcdownload_element['onclick'].split("'")[1]
+        dlc_url = f"https://{filecrypt_url.split('/')[2]}/DLC/{dlc_id}.dlc"
+        dlc_response = requests.get(dlc_url)
+        dcrypt_url = "http://dcrypt.it/decrypt/paste"
+        dcrypt_data = {"content": dlc_response.text}
+        dcrypt_response = requests.post(dcrypt_url, data=dcrypt_data)
+        dcrypt_json = json.loads(dcrypt_response.text)
+        return dcrypt_json['success']['links']
 
 dp = Dispatcher(bot, None, use_context=True)
 dp.add_handler(CommandHandler("start", start))
