636e02fb0a531e71c49930719837c6493ad36642
lasoffitta <165275950+lasoffitta@users.noreply.github.com>
2024-04-01 02:08:46 +0200

    Update filecrypt.py

--- filecrypt.py
+++ filecrypt.py
@@ -51,10 +51,6 @@ dp.add_handler(CommandHandler("start", start))
 dp.add_handler(CommandHandler("link", process_links))
 dp.add_handler(MessageHandler(Filters.text & ~Filters.command, process_links))
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-}
-
 def get_links(filecrypt_url):
     logging.debug(f"Processing {filecrypt_url}...")
     response = requests.get(filecrypt_url, headers=headers)
